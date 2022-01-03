32d9e4875b0906354463936baaa3257e9c311ec1
mareadelmar <mar.ortega.v@gmail.com>
2022-01-03 17:19:09 -0300

    Create navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-	return <div className='App'>holis</div>;
+	return (
+		<div className='App'>
+			<Navbar />
+			<h1>holis</h1>
+		</div>
+	);
 }
 
 export default App;
