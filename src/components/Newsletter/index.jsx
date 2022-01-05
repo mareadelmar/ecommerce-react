247916948276f20400910fd039ce125492f16b11
mareadelmar <mar.ortega.v@gmail.com>
2022-01-05 18:17:54 -0300

    Create newsletter component

---
+++ src/components/Newsletter/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Title, Text, InputContainer, Input } from "./styles";
+
+const Newsletter = () => {
+	return (
+		<Container>
+			<Title></Title>
+			<Text></Text>
+			<InputContainer>
+				<Input />
+			</InputContainer>
+		</Container>
+	);
+};
+
+export default Newsletter;
---
+++ src/components/Newsletter/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Title = styled.h3``;
+
+export const Text = styled.p``;
+
+export const InputContainer = styled.div``;
+
+export const Input = styled.input``;
