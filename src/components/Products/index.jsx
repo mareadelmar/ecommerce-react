eff4dd82bf6163dcad8bc582dc61c0cfdcd0643c
mareadelmar <mar.ortega.v@gmail.com>
2022-01-07 16:02:55 -0300

    Add and style the link to products page

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Container } from "./styles";
+import { Container, Wrapper, Title, Link } from "./styles";
 import { popularProducts } from "../../db";
 import ProductItem from "../ProductItem";
 
 const Categories = () => {
 	return (
 		<Container>
-			{popularProducts.map(item => (
-				<ProductItem product={item} key={item.id} />
-			))}
+			<Link to='products'>
+				<Title>OUR PRODUCTS</Title>
+			</Link>
+			<Wrapper>
+				{popularProducts.map(item => (
+					<ProductItem product={item} key={item.id} />
+				))}
+			</Wrapper>
 		</Container>
 	);
 };
--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,7 +1,22 @@
 import styled from "styled-components";
+import { Link as LinkRouter } from "react-router-dom";
 
 export const Container = styled.div`
 	min-height: 90vh;
+`;
+
+export const Link = styled(LinkRouter)`
+	text-decoration: none;
+	color: black;
+`;
+
+export const Title = styled.h1`
+	font-weight: 300;
+	text-align: center;
+	margin: 1em 0;
+`;
+
+export const Wrapper = styled.div`
 	display: flex;
 	flex-wrap: wrap;
 `;
